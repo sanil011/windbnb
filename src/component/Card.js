4de98b739dc0a7894a7af43b16c333341be6043b
sanil011 <sanilp191@gmail.com>
2022-12-26 02:00:53 +0530

    remove the bug of when we click on arrow button but not showing other photo

--- src/component/Card.js
+++ src/component/Card.js
@@ -1,42 +1,43 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import Right from '../assest/rightarrow.svg';
 import Left from '../assest/leftarrow.svg';
 import { Link } from 'react-router-dom';
 import { detail } from '../redux/action';
 import { useDispatch } from 'react-redux';
-const Card = ({ photo, photo1, superHost, type, star, title ,data}) => {
+const Card = ({ photo, photo1, superHost, type, star, title, data }) => {
   const [currentSlide, setCurrentSlide] = useState(0);
   const dispatch = useDispatch();
 
-  return (  
+  return (
 
     <div onClick={() => { console.log("sanil"); dispatch(detail(data)) }} className='mobile:w-1/2 animate-slideup tablet:w-1/3 laptop:w-1/4 aspect-square p-4 mb-4 '>
       <div className='w-full '>
-        <Link to='/detail'>
         <div className='relative  group hover'>
-          <section className={`${!currentSlide ? "block":" hidden"}`}>
-            <img className='rounded-2xl object-cover my-2 aspect-[1/.7] ' src={photo} alt="src" /> 
-          </section>
-          <section className={`${currentSlide ? "block":" hidden"} `}>
-            <img className='rounded-2xl object-cover my-2 aspect-[1/.7]' src={photo1} alt="src" />  
-          </section>
+          <Link to='/detail'>
+            <section className={`${!currentSlide ? "block" : " hidden"}`}>
+              <img className='rounded-2xl object-cover my-2 aspect-[1/.7] ' src={photo} alt="src" />
+            </section>
+            <section className={`${currentSlide ? "block" : " hidden"} `}>
+              <img className='rounded-2xl object-cover my-2 aspect-[1/.7]' src={photo1} alt="src" />
+            </section>
+          </Link>
           <div className='invisible group-hover:visible'>
-             <img className='cursor-pointer absolute top-1/2 rounded-lg left-4   bg-white opacity-80 ' onClick={() => setCurrentSlide(!currentSlide)} src={Left} alt="arrow"/> 
-             <img className='cursor-pointer absolute top-1/2 rounded-lg   right-4 bg-white opacity-80 ' onClick={()=>setCurrentSlide(!currentSlide)} src={Right} alt="arrow"/>
+            <img className='cursor-pointer absolute top-1/2 rounded-lg left-4   bg-white opacity-80 ' onClick={() => setCurrentSlide(!currentSlide)} src={Left} alt="arrow" />
+            <img className='cursor-pointer absolute top-1/2 rounded-lg   right-4 bg-white opacity-80 ' onClick={() => setCurrentSlide(!currentSlide)} src={Right} alt="arrow" />
           </div>
           <div>
-             <span className={`w-2 h-2 rounded-lg ${currentSlide ? "bg-white opacity-80 ":" bg-gray-400 opacity-50"}  absolute top-[90%] left-[43%]`} ></span>
-            <span className={`w-2 h-2 rounded-lg ${!currentSlide ? "bg-white opacity-80 ":" bg-gray-400 opacity-50"} absolute top-[90%] left-[50%]`}></span>
+            <span className={`w-2 h-2 rounded-lg ${currentSlide ? "bg-white opacity-80 " : " bg-gray-400 opacity-50"}  absolute top-[90%] left-[43%]`} ></span>
+            <span className={`w-2 h-2 rounded-lg ${!currentSlide ? "bg-white opacity-80 " : " bg-gray-400 opacity-50"} absolute top-[90%] left-[50%]`}></span>
           </div>
-      </div>
-          <div className='flex justify-between'>
+        </div>
+        <div className='flex justify-between'>
           {superHost && <h6 className='font-bold text-sm text-[#4F4F4F] border-2 py-0.5 px-2 rounded-2xl'>SUPER HOST</h6>}
           <p className='font-medium text-sm text-[#828282]'>{type}</p>
-          <p>⭐️  { star}</p>
-          </div>
-          <p className='text-[#333333] text-base'>{ title}</p></Link>
+          <p>⭐️  {star}</p>
         </div>
+        <p className='text-[#333333] text-base'>{title}</p>
       </div>
+    </div>
 
   )
 }
